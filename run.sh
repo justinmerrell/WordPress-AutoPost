d528c81d804fdda174f1d569b83f1d571ab5ae20
Justin Merrell <merrelljustin@gmail.com>
2023-04-17 09:49:27 -0400

    added run script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source /root/automation/WordPress-AutoPost/env/bin/activate
+
+# Call the main.py script
+python main.py
+
+# Deactivate the virtual environment
+deactivate
